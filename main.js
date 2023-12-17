4722fee0840c9cb50e59b53562ffe3ceee3ccaab
luiz-ssantos <luiz.ssantos@outlook.com.br>
2023-12-17 10:44:11 -0300

    Adicionando barra de status

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   // Obter elementos do DOM
   var buttonValidacao = document.getElementById("buttonValidacao");
   var img = document.getElementById("imgValidacao");
+  var txt = document.getElementById("textoValidacao");
 
   // Adicionar evento de clique ao botão de validação
   buttonValidacao.addEventListener("click", function (event) {
@@ -13,6 +14,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Verificar se os valores são números
     if (!isNaN(numPrimeiro) && !isNaN(numSegundo)) {
+      txt.style.display = "block";
       // Verificar se o número B é maior que o número A
       if (numSegundo > numPrimeiro) {
         // Exibir mensagem de validação positiva
@@ -20,16 +22,22 @@ document.addEventListener("DOMContentLoaded", function () {
           `Parabens o formulario foi validado!\n o numero ${numSegundo} é maior que ${numPrimeiro}`
         );
         img.src = "img/002.png"; // Atualizar a imagem
+        txt.innerHTML = `O valor B (${numSegundo}) é maior que o valor A (${numPrimeiro})`;
+        txt.style.backgroundColor = "#00bd29";
       } else {
         // Exibir mensagem de validação negativa
         alert(
           `Desculpe o formulario foi preenchido de forma inválido!\nO numero ${numSegundo} não é maior que ${numPrimeiro}.\nTente novamente!`
         );
         img.src = "img/003.png"; // Atualizar a imagem
+        txt.innerHTML = `O valor B (${numSegundo}) não é maior que o valor A (${numPrimeiro})`;
+        txt.style.backgroundColor = "#ff0000";
       }
     } else {
       // Exibir mensagem de erro para números inválidos
       alert("Por favor, insira números válidos.");
+      img.src = "img/003.png"; // Atualizar a imagem
+      txt.style.backgroundColor = "#ff0000";
     }
   });
 });
